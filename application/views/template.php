22507be159f189b0079bd6ac007e298f23921996
r171n <yudhanovisusanto@gmail.com>
2020-02-12 19:05:18 +0700

    Edit template dan layout view

---
+++ application/views/template.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php echo $__header; ?>
+<body>
+<?php $typeuser = $this->session->userdata('login'); ?>
+<?php echo $__menu; ?>
+<?php echo $__content; ?>
+</div>
+<?php echo $__footer; ?>
+</body>
+</html>
